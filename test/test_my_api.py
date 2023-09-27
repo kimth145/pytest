1214e6be03a51acf70bf4ec210ba638f296a1778
kimth <kimth145@ais-info.co.jp>
2023-09-27 20:56:29 +0900

    6 commit

--- test/test_my_api.py
+++ test/test_my_api.py
@@ -11,4 +11,4 @@ def client():
 def test_login(client):
     response = client.get("/test/1/2")
     assert response.status_code == 200
-    assert response.json() == 2
+    assert response.json() == 3
